02652fc6ea2fdef06bacd6bf3c71f1b818693043
osvaldomiranda <osvaldo.omiranda@gmail.com>
2019-08-20 22:59:16 -0400

    [ADD] focuser stop monitor

--- python_programs/focuserController.py
+++ python_programs/focuserController.py
@@ -1,35 +1,37 @@
 import requests
 import json
 import subprocess
+import os
 from subprocess import call
 
 
-ip = '192.168.0.102'
+ip = '54.70.235.195'
 url = 'http://'+ip+'/api/command/focus'
 data = '{}'
 response = requests.get(url, data=data)
-
+cwd = os.getcwd()
 
 if(response.ok):
 
     jData = json.loads(response.content)
 
-    print( jData['command'])
+    print( jData)
 
     if(jData['type']=='focuser'):
-        steps = str(['steps'])
-        direction= str(['direction'])
+        steps = str(jData['steps'])
+        direction= str(jData['direction'])
 
         url = 'http://'+ip+'/api/messages/send?sender_id=1&receiver_id=2&message=Comando Enfocador recibido'
         data = '{}'
         response = requests.post(url, data=data)
 
         comando = "/usr/bin/indi_setprop 'Pegasus DMFC.ABS_FOCUS_POSITION.FOCUS_ABSOLUTE_POSITION="+steps+"'"
-
+        print(comando)
         try:
             url = 'http://'+ip+'/api/messages/send?sender_id=1&receiver_id=2&message=Comenzando Enfoque'
             data = '{}'
             response = requests.post(url, data=data)            
             subprocess.check_output(comando, shell=True,stderr=subprocess.STDOUT)
+            call(["/usr/bin/python3", cwd + "/focuserStopMonitor.py"])
         except subprocess.CalledProcessError as e:
             raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
---
+++ python_programs/fosuserStopMonitor.py
@@ -0,0 +1,54 @@
+import subprocess
+import threading
+import time
+import requests
+import os
+from subprocess import call
+
+global i
+ip = '54.70.235.195'
+cwd = os.getcwd()
+def printit():
+    t = threading.Timer(2, printit)
+    t.start()
+
+    comando = 'indi_getprop "Pegasus DMFC.ABS_FOCUS_POSITION.FOCUS_ABSOLUTE_POSITION"'
+
+    try:
+        b_ra = subprocess.check_output(comando, shell=True,stderr=subprocess.STDOUT)
+        c_ra = b_ra.decode("utf-8")
+        ra = round( float(c_ra.split('=',1)[1]),2)
+        print(ra)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+
+    global ra_ant
+    global data 
+    if ra==ra_ant :
+        t.cancel()
+        print('listo')    
+        url = 'http://'+ip+'/api/messages/send?sender_id=1&receiver_id=2&message=Enfocador OK '
+        data = '{}'
+        response = requests.post(url, data=data)
+    else :
+        ra_ant  = ra
+        url = 'http://'+ip+'/api/messages/send?sender_id=1&receiver_id=2&message=Pos.Actual :' + str(ra)
+        data = '{}'
+        response = requests.post(url, data=data)
+
+
+comando = 'indi_getprop "Pegasus DMFC.ABS_FOCUS_POSITION.FOCUS_ABSOLUTE_POSITION"'
+try:
+    b_ra = subprocess.check_output(comando, shell=True,stderr=subprocess.STDOUT)
+    c_ra = b_ra.decode("utf-8")
+    ra_ant = round( float(c_ra.split('=',1)[1]),2)
+    # print(ra_ant)
+except subprocess.CalledProcessError as e:
+    raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+
+print("valores de RA ")
+print(ra_ant)
+time.sleep(3) 
+printit()
